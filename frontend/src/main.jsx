bb5d7cbe2044ad91056e19a9aa9055b5bc00d685
Indrooneel Panday <indrooneelpanday@gmail.com>
2026-05-21 20:21:31 +0530

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MIGCoreDashboard from './MIGCoreDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MIGCoreDashboard />
+  </React.StrictMode>
+)
